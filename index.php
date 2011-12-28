21772333c4864e1c3640edfea2540ccd712d51a9
Andrew Latham <acl68@case.edu>
2011-12-27 20:11:56 -0500

    Index, CSS Sheet

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Where's It Made?</title>
+<link rel="stylesheet" type="text/css" href="/sheet1.css" />
+<script src="http://www.lathamcity.com/_js/jquery-1.7.js></script>
+</head>
+<body>
+<?php
+	echo "Test"
+?>
+</body>
+</html>
